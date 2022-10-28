90ebe6b82570f84ad1d205a248d92c843ec23036
Rayen ouji <101173384+Rayenouji@users.noreply.github.com>
2022-10-28 20:47:10 +0200

    Add files via upload

---
+++ projetpfe/src/main/java/com/example/demo/entities/FilmProjection.java
@@ -0,0 +1,7 @@
+package com.example.demo.entities;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "nomProd", types = { Film.class })
+public interface FilmProjection {
+public String getNomFilmt();
+}
